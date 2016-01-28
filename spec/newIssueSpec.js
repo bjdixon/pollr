dae3a26123ad98fabb4d0902188540ad40d05270
Brian Dixon <bjdixon@gmail.com>
2016-01-27 22:35:41 -0500

    update comment urls in fixtures and extend timeout on tests

--- spec/newIssueSpec.js
+++ spec/newIssueSpec.js
@@ -4,7 +4,7 @@ const server = require('../server/server.js').createServer(3000);
 
 describe('New Issue Endpoint', function () {
 
-  it('responds with status code 200 and new poll when supplied a new poll issue', function() {
+  it('responds with status code 200 and new poll when supplied a new poll issue', function(done) {
     let options = {
       method: 'POST',
       url: '/new-issue',
@@ -13,10 +13,11 @@ describe('New Issue Endpoint', function () {
     server.inject(options, function(response) {
       expect(response.statusCode).toBe(200);
       expect(response.result).toBe('new poll');
+      done();
     });
-  });
+  }, 15000);
 
-  it('responds with status code 200 and not new poll when not a new poll issue', function() {
+  it('responds with status code 200 and not new poll when not a new poll issue', function(done) {
     let options = {
       method: 'POST',
       url: '/new-issue',
@@ -25,6 +26,7 @@ describe('New Issue Endpoint', function () {
     server.inject(options, function(response) {
       expect(response.statusCode).toBe(200);
       expect(response.result).toBe('not new poll');
+      done();
     });
-  });
+  }, 15000);
 });
